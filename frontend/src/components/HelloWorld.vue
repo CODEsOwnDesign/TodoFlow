9e4266da91024cce89ed55b58b9a81bfc0def3f9
ECHO's Odyssey <145453820+Gideon-Yebei@users.noreply.github.com>
2024-05-13 16:01:42 +0300

    initial commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Documentation </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <MDBBtn class="mb-3" color="primary" @click="state.count++"> count is: {{ state.count }}</MDBBtn>
+
+  <p>Edit components/HelloWorld.vue to test hot module replacement.</p>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, reactive } from 'vue'
+import { MDBBtn } from 'mdb-vue-ui-kit'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
